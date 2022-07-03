78945e61d7b40c527d7d79220d8a06764bf043d4
Onixstar <83131460+Onixstar@users.noreply.github.com>
2022-07-03 16:20:55 +0200

    FitnessCrossOver Initial Draft

--- client/src/main/java/org/evosuite/ga/operators/crossover/FitnessCrossOver.java
+++ client/src/main/java/org/evosuite/ga/operators/crossover/FitnessCrossOver.java
@@ -2,6 +2,7 @@ package org.evosuite.ga.operators.crossover;
 
 import org.evosuite.ga.Chromosome;
 import org.evosuite.ga.ConstructionFailedException;
+import org.evosuite.utils.Randomness;
 
 public class FitnessCrossOver extends CrossOverFunction {
     @Override
@@ -10,16 +11,21 @@ public class FitnessCrossOver extends CrossOverFunction {
         if (parent1.size() < 2 || parent2.size() < 2) {
             return;
         }
+          
+        // Get Fitness values of chromosones
+        double fitness1 = parent1.getFitness();
+        double fitness2 = parent2.getFitness();
+        double ratio = Math.max(0.2, Math.min(fitness1 / fitness2, 1.8));
         
-        // Determine CrossOver-Points based on fitness values of the chromosones
-        
-        int middle1 = (int) Math.round(parent1.size() / 2.0);
-        int middle2 = (int) Math.round(parent2.size() / 2.0);
-        
+        // Clone for Crossover operation
         Chromosome t1 = parent1.clone();
         Chromosome t2 = parent2.clone();
         
-        parent1.crossOver(t2, middle1, middle2);
-        parent2.crossOver(t1, middle2, middle1);
+        // Determine Crossover Points based on fitness values of the chromosones
+        int point1 = (int) Math.round(parent1.size() * ratio / 2.0);
+        int point2 = (int) Math.round(parent2.size() * (1.0 - (ratio / 2.0)));
+        
+        parent1.crossOver(t2, point1, point2);
+        parent2.crossOver(t1, point2, point1);
     }
 }
