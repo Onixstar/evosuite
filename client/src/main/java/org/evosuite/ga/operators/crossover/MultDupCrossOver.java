98201e49046110211b2fba4e11f1b632d6aa7394
Opticus10 <104192356+Opticus10@users.noreply.github.com>
2022-07-09 12:01:20 +0200

    Updated calculations on MultDupCrossover.java

--- client/src/main/java/org/evosuite/ga/operators/crossover/MultDupCrossOver.java
+++ client/src/main/java/org/evosuite/ga/operators/crossover/MultDupCrossOver.java
@@ -53,55 +53,38 @@ public class MultDupCrossOver<T extends Chromosome<T>> extends CrossOverFunction
         double fitness_1 = parent1.getFitness();
         double fitness_2 = parent2.getFitness();
         int point1 = 0;
-        int point2 = 0;
 
         // Clone for crossover
         T t1 = parent1.clone();
         T t2 = parent2.clone();
 
-        // Calculate threshold for iteration count based on fitness
+        // Calculate ratio for iteration count based on fitness
         // Higher differences result in more copies of the chromosome with higher fitness value
-        double threshold = 0;
+        // The ratio will always be >= 1
+        double ratio = 0;
+        T fittest_parent = null;
         if(fitness_1 > fitness_2){
-            threshold = fitness_1 / fitness_2;
+            ratio = fitness_1 / fitness_2;
+            fittest_parent = parent1.clone();
         } 
         else {
-            threshold = fitness_2 / fitness_1;
+            ratio = fitness_2 / fitness_1;
+            fittest_parent = parent1.clone();
         }
 
-        // Set iteration length based on threshold, which is cut off at a maximum iteration
-        // count of 10 * threshold, limiting threshold size too
-        int max_len = 0;
-        if(threshold > 10){
-            max_len = (int) Math.ceil(10 * Math.min(10,(parent1.size() + parent2.size())/2));
-        }
-        else{
-            max_len = (int) Math.ceil(threshold * Math.min(10,(parent1.size() + parent2.size())/2));
-        }
+        // Set iteration length based on ratio, which is cut off at a maximum iteration
+        // Capping iteration limit at 1000 iterations
+        int max_len = (int) Math.min(Math.ceil(ratio * fittest_parent.size()), 1000);
 
-        // Choose parent with higher fitness
-        if(fitness_1 > fitness_2){
-            // Threshold influences points also in each iteration, so that more genes from
-            // a chromosome wit higher fitness are inherited
-            // An additional random factor makes sure we have different crossover points each iteration
-            for(int i = 0; i < max_len; i++){
-                point1 = (int) (Randomness.nextInt(parent1.size()) + (int) Math.min(Math.floor(parent1.size()*threshold), parent1.size()) - 2)/2;
-                point2 = (int) (Randomness.nextInt(parent2.size()) - 1);
-                // Using Single-point-crossover function provided by chromosome class
-                parent1.crossOver(t2, point1, point2);
-                parent2.crossOver(t1, point2, point1);
-            } 
-        }
-        else{
-            // Same as above, but for second parent
-            for(int i = 0; i < max_len; i++){
-                point1 = (int) (Randomness.nextInt(parent1.size()) -1);
-                point2 = (int) (Randomness.nextInt(parent2.size()) + (int) Math.min(Math.floor(parent2.size()*threshold), parent2.size()) - 2)/2;
-                // Using Single-point-crossover function provided by chromosome class
-                parent1.crossOver(t2, point1, point2);
-                parent2.crossOver(t1, point2, point1);
-            } 
-        }
+        // Threshold influences points also in each iteration, so that more genes from
+        // a chromosome wit higher fitness are inherited
+        // An additional random factor makes sure we have different crossover points each iteration ()
+        int max_genes = Math.min(parent1.size(), parent2.size());
+        for(int i = 0; i < max_len; i++){
+            point1 = (int) ((Randomness.nextInt(max_genes) - 1) + ((max_genes * (int) Math.min((Math.ceil((fitness_1 * fitness_2)/(fitness_1 + fitness_2))),1)) - 1))/2;
+            // Using Single-point-crossover function provided by chromosome class
+            parent1.crossOver(t2, point1);
+            parent2.crossOver(t1, point1);
+        } 
     }
-
 }
