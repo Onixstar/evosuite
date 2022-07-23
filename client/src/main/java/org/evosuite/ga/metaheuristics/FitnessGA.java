0d452264a97dc2e7785ff039b657463711bad218
Alexander Reinicke <83131460+Onixstar@users.noreply.github.com>
2022-07-23 18:07:27 +0200

    Update FitnessGA.java

--- client/src/main/java/org/evosuite/ga/metaheuristics/FitnessGA.java
+++ client/src/main/java/org/evosuite/ga/metaheuristics/FitnessGA.java
@@ -74,7 +74,7 @@ public class FitnessGA<T extends Chromosome<T>> extends StandardGA<T> {
         
         Double median = 0.0;
         if(Properties.MEDIAN == true) { // MEDIAN
-            for(int i = 0; i < FitnessValues.size() / 2; i++) {
+            for(int i = 0; i <= FitnessValues.size() / 2; i++) {
                 median = FitnessValues.get(i);
             }
         }
